51b179374d9875a917696450d6d0abbce7dae51f
Frankwii <frank.william.hammond@gmail.com>
2025-05-15 18:44:35 +0200

    Added a description and repo url to the project

--- tests/test_tracking.py
+++ tests/test_tracking.py
@@ -1,32 +1,37 @@
 import torch
 
-from u2fold.utils.track import get_tracked_group, track
+from u2fold.utils.track import get_tag_group, tag
 
 
 def test_cli_common_command_tracking():
 
-    args = get_tracked_group("cli_argument/common")
+    args = get_tag_group("cli_argument/common")
 
-    expected_args = {"log_level"}
+    expected_args = {"log_level", "log_dir"}
 
     assert expected_args == set(args.keys())
 
 def test_cli_command_tracking():
 
-    args = get_tracked_group("cli_argument")
+    args = get_tag_group("cli_argument")
 
     expected_args = {"common", "exec", "train"}
 
     print(args)
     assert expected_args == set(args.keys())
 
-@track(tag="model/mock")
+@tag("model/mock")
 class MockModel(torch.nn.Module):
     ...
 
 def test_mock_model_tracking():
 
-    models = get_tracked_group("model")
+    models = get_tag_group("model")
 
     assert "mock" in models.keys()
 
+def test_models_tracking():
+
+    models = get_tag_group("model")
+
+    assert {"unet-like"}.issubset(set(models.keys()))
